7c86c02eec639dab936bb84c2559e469c3096aef
blackboob <2439027462@qq.com>
2020-04-29 14:08:05 +0800

    commit: 提交本地

--- src/main/java/com/testwithgit/demo/model/Student.java
+++ src/main/java/com/testwithgit/demo/model/Student.java
@@ -2,6 +2,15 @@ package com.testwithgit.demo.model;
 
 public class Student {
     private String stuName;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public String getStuName() {
         return stuName;
