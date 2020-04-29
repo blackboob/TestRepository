50226d1023308fb917fe822e54215a3286de0e79
blackboob <2439027462@qq.com>
2020-04-29 13:50:38 +0800

    commit-2:student model

---
+++ src/main/java/com/testwithgit/demo/model/Student.java
@@ -0,0 +1,13 @@
+package com.testwithgit.demo.model;
+
+public class Student {
+    private String stuName;
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+}
