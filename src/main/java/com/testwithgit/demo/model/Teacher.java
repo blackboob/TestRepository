a4467d008c3585294bca2f7bf94606d57ff4c4de
blackboob <2439027462@qq.com>
2020-04-29 14:10:28 +0800

    commit: 提交本地teacher

---
+++ src/main/java/com/testwithgit/demo/model/Teacher.java
@@ -0,0 +1,13 @@
+package com.testwithgit.demo.model;
+
+public class Teacher {
+    private String teaName;
+
+    public String getTeaName() {
+        return teaName;
+    }
+
+    public void setTeaName(String teaName) {
+        this.teaName = teaName;
+    }
+}
